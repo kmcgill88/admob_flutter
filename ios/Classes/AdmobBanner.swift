46b49ecfb3ee6c3fabfad856e26e8807694918df
Kevin McGill <kevin@mcgilldevtech.com>
2019-05-20 20:23:08 -0500

    WIP: banners

--- ios/Classes/AdmobBanner.swift
+++ ios/Classes/AdmobBanner.swift
@@ -1,8 +1,141 @@
-//
-//  AdmobBanner.swift
-//  admob_flutter
-//
-//  Created by Youssef Kababe on 12/9/18.
-//
+/*
+ Copyright (c) 2019 Kevin McGill <kevin@mcgilldevtech.com>
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 
+import Flutter
 import Foundation
+import GoogleMobileAds
+
+class AdmobBanner : NSObject, FlutterPlatformView {
+
+    private let channel: FlutterMethodChannel
+    private let messeneger: FlutterBinaryMessenger
+    private let frame: CGRect
+    private let viewId: Int64
+    private let args: [String: Any]
+    private var adView: GADBannerView?
+
+    init(frame: CGRect, viewId: Int64, args: [String: Any], messeneger: FlutterBinaryMessenger) {
+        self.args = args
+        self.messeneger = messeneger
+        self.frame = frame
+        self.viewId = viewId
+        channel = FlutterMethodChannel(name: "admob_flutter/banner_\(viewId)", binaryMessenger: messeneger)
+    }
+    
+    func view() -> UIView {
+        return getBannerAdView() ?? UIView()
+    }
+
+    fileprivate func dispose() {
+        adView?.removeFromSuperview()
+        adView = nil
+        channel.setMethodCallHandler(nil)
+    }
+    
+    fileprivate func getBannerAdView() -> GADBannerView? {
+        if adView == nil {
+            adView = GADBannerView()
+            adView!.rootViewController = UIApplication.shared.keyWindow?.rootViewController
+            adView!.delegate = self
+            adView!.frame = self.frame.width == 0 ? CGRect(x: 0, y: 0, width: 1, height: 1) : self.frame
+            adView!.adUnitID = self.args["adUnitId"] as? String ?? "ca-app-pub-3940256099942544/6300978111"
+            adView!.delegate = self
+            channel.setMethodCallHandler { [weak self] (flutterMethodCall: FlutterMethodCall, flutterResult: FlutterResult) in
+                switch flutterMethodCall.method {
+                case "setListener":
+//                    self?.adView?.delegate = self
+                    break
+                case "dispose":
+                    self?.dispose()
+                    break
+                default:
+                    flutterResult(FlutterMethodNotImplemented)
+                }
+            }
+            requestAd()
+        }
+        
+        return adView
+    }
+    
+    fileprivate func requestAd() {
+        if let ad = getBannerAdView() {
+            let request = GADRequest()
+//            if debug {
+                request.testDevices = [kGADSimulatorID]
+//            }
+            ad.load(request)
+        }
+    }
+    
+    fileprivate func getSize() -> GADAdSize {
+        let size = args["adSize"] as? [String: Any]
+        let width = size!["width"] as? Int ?? 0
+        let height = size!["height"] as? Int ?? 0
+        let name = size!["name"] as! String
+        
+        switch name {
+        case "BANNER":
+            return kGADAdSizeBanner
+        case "LARGE_BANNER":
+            return kGADAdSizeLargeBanner
+        case "MEDIUM_RECTANGLE":
+            return kGADAdSizeMediumRectangle
+        case "FULL_BANNER":
+            return kGADAdSizeFullBanner
+        case "LEADERBOARD":
+            return kGADAdSizeLeaderboard
+        case "SMART_BANNER":
+            // TODO: Do we need Landscape too?
+            return kGADAdSizeSmartBannerPortrait
+        default:
+            return GADAdSize.init(size: CGSize(width: width, height: height), flags: 0)
+        }
+    }
+
+}
+
+extension AdmobBanner : GADBannerViewDelegate {
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        channel.invokeMethod("loaded", arguments: nil)
+    }
+    
+    func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+         channel.invokeMethod("failedToLoad", arguments: error)
+    }
+    
+    /// Tells the delegate that a full screen view will be presented in response to the user clicking on
+    /// an ad. The delegate may want to pause animations and time sensitive interactions.
+    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
+        channel.invokeMethod("clicked", arguments: nil)
+        channel.invokeMethod("opened", arguments: nil)
+    }
+    
+    // channel.invokeMethod("impression", null)
+    
+    func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
+        channel.invokeMethod("leftApplication", arguments: nil)
+    }
+    
+    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
+        channel.invokeMethod("closed", arguments: nil)
+    }
+}
--- ios/Classes/SwiftAdmobFlutterPlugin.swift
+++ ios/Classes/SwiftAdmobFlutterPlugin.swift
@@ -1,14 +1,61 @@
+/*
+ Copyright (c) 2019 Kevin McGill <kevin@mcgilldevtech.com>
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
 import Flutter
 import UIKit
+import GoogleMobileAds
 
 public class SwiftAdmobFlutterPlugin: NSObject, FlutterPlugin {
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "admob_flutter", binaryMessenger: registrar.messenger())
     let instance = SwiftAdmobFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    let defaultChannel = FlutterMethodChannel(name: "admob_flutter", binaryMessenger: registrar.messenger())
+    registrar.addMethodCallDelegate(instance, channel: defaultChannel)
+
+    let interstitialChannel = FlutterMethodChannel(name: "admob_flutter/interstitial", binaryMessenger: registrar.messenger())
+    registrar.addMethodCallDelegate(instance, channel: interstitialChannel)
+
+    let rewardChannel = FlutterMethodChannel(name: "admob_flutter/reward", binaryMessenger: registrar.messenger())
+    registrar.addMethodCallDelegate(instance, channel: rewardChannel)
+
+    registrar.register(
+        AdmobBannerFactory(messeneger: registrar.messenger()),
+        withId: "admob_flutter/banner"
+    )
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "initialize":
+        GADMobileAds.sharedInstance().start { (initializationStatus: GADInitializationStatus) in
+            print("initializationStatus:")
+            print(initializationStatus)
+        }
+        break
+    case "getPlatformVersion":
+        result("iOS " + UIDevice.current.systemVersion)
+        break
+    default:
+        result("success")
+//        result(FlutterMethodNotImplemented)
+    }
   }
 }
