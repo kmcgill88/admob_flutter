aed1d21b6ce9a8cafbab7c1885d3c360fc217f31
Kevin McGill <kevin@mcgilldevtech.com>
2020-08-05 22:29:20 -0500

    bump dart min to 2.7 for extensions!

--- example/lib/new_page.dart
+++ example/lib/new_page.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-
 import 'package:admob_flutter_example/extensions.dart';
+import 'package:flutter/material.dart';
 
 class NewPage extends StatelessWidget {
   final String title;
@@ -8,7 +7,7 @@ class NewPage extends StatelessWidget {
     Key key,
     @required this.title,
   }) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +15,9 @@ class NewPage extends StatelessWidget {
         title: Text(title),
         brightness: Brightness.dark,
       ).withBottomAdmobBanner(context),
+      body: Container(
+        color: Colors.green,
+      ),
     );
   }
 }
