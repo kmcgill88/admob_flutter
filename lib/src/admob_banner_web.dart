b969bbc64b541893c67efbd0ad912ef6efbb5b89
Kevin McGill <kevin@mcgilldevtech.com>
2020-08-13 21:15:38 -0500

    Move script

--- lib/src/admob_banner_web.dart
+++ lib/src/admob_banner_web.dart
@@ -31,34 +31,46 @@ class _AdmobBannerState extends State<AdmobBanner> {
   @override
   Widget build(BuildContext context) {
     final theKey = _key.toString();
+    print('build' + theKey);
 
-    final key1 = theKey + 'script1';
-    final script1 = html.ScriptElement()
-      ..type = 'text/javascript'
-      ..async = true
-      ..src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    // final key1 = theKey + 'script1';
+    // final script1 = html.ScriptElement()
+    //   ..type = 'text/javascript'
+    //   // ..async = true
+    //   ..src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+    //   ..addEventListener('load', (event) {
+    //     print('loaded111');
+    //   });
 
     final key2 = theKey + 'script2';
     final script2 = html.ScriptElement()
-      ..setInnerHtml(
-          '(adsbygoogle = window.adsbygoogle || []).push({});console.log("adsense loaded")')
-      ..type = 'text/javascript';
+      ..type = 'text/javascript'
+      ..setInnerHtml('''
+      var adsbygoogle = window.adsbygoogle || [];
+      console.log("isLoaded: ", adsbygoogle);
+      if (adsbygoogle.loaded) {
+        console.log("adsense loaded. nothing to do here.");
+      } else {
+        console.log("needs to load")
+        adsbygoogle.push({});
+      }
+      ''');
 
-    final foo = html.Element.tag('ins')
-      ..className = 'adsbygoogle'
-      ..style.display = 'block'
+    final insTag = html.Element.tag('ins')
       ..attributes = {
+        'class': 'adsbygoogle',
+        'style': 'display:block',
         'data-ad-client': 'ca-pub-7570377070409946',
         'data-ad-slot': '2851567112',
-        // 'data-ad-format': 'auto',
+        'data-ad-format': 'auto',
         'data-full-width-responsive': 'true',
       };
 
     final divElement = html.DivElement()
       ..style.backgroundColor = 'red'
       ..style.border = 'none'
-      ..append(script1)
-      ..append(foo)
+      // ..append(script1)
+      ..append(insTag)
       ..appendHtml('Hello Flutter Web')
       ..append(script2);
 
@@ -70,12 +82,12 @@ class _AdmobBannerState extends State<AdmobBanner> {
       },
     );
     // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(
-      key1,
-      (int viewId) {
-        return script1;
-      },
-    );
+    // ui.platformViewRegistry.registerViewFactory(
+    //   key1,
+    //   (int viewId) {
+    //     return script1;
+    //   },
+    // );
     // ignore: undefined_prefixed_name
     ui.platformViewRegistry.registerViewFactory(
       key2,
