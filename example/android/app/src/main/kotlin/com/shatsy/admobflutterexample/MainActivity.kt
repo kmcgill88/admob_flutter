115ab0423683a45aef91c6d2c5db8f5f6c8ce3bc
Alan Trope <atrope@gmail.com>
2020-10-02 17:20:15 -0300

    upgrade example app

--- example/android/app/src/main/kotlin/com/shatsy/admobflutterexample/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/shatsy/admobflutterexample/MainActivity.kt
@@ -1,13 +1,3 @@
 package com.shatsy.admobflutterexample
-
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
-}
+import io.flutter.embedding.android.FlutterActivity;
+public class MainActivity extends FlutterActivity { }
